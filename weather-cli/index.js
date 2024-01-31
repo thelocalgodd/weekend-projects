cece8bfc68f7b564f32a4c93e92e0955d308c0c2
thelocalgodd <vkkpemlie@st.knust.edu.gh>
2024-01-31 10:03:54 +0000

    changed chalk colors for weather-cli

--- weather-cli/index.js
+++ weather-cli/index.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import fetch from "node-fetch";
 
-console.log(chalk.bgWhite("Weather App"));
+console.log(chalk.bgBlueBright("Weather App"));
 console.log("------------");
 console.log(
   chalk.bgCyan("Enter your location to get the weather details\n") +
@@ -16,16 +16,19 @@ console.log(
 let location = prompt("Enter your location: ");
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.API_KEY}&units=metric`;
-
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    // output the location, weather, humidity, and temperature
-    console.log("\n------------------------------------");
-    console.log(`Location: ` + chalk.bgWhite(`${data.name}`));
-    console.log("------------------------------------");
-    console.log(`Weather: ${data.weather[0].main}`);
-    console.log(`Humidity: ${data.main.humidity} %`);
-    console.log(`Temperature: ${data.main.temp} °C`);
-    console.log("------------------------------------\n");
-  });
+try {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // output the location, weather, humidity, and temperature
+      console.log("\n------------------------------------");
+      console.log(`Location: ` + chalk.bgBlueBright(`${data.name}`));
+      console.log("------------------------------------");
+      console.log(`Weather: ${data.weather[0].main}`);
+      console.log(`Humidity: ${data.main.humidity} %`);
+      console.log(`Temperature: ${data.main.temp} °C`);
+      console.log("------------------------------------\n");
+    });
+} catch (error) {
+  console.log(error);
+}
